ccee6ffc2e23c1abf007a62797923d4e6598d37d
geogeorge1997 <igeogeorge1997@gmail.com>
2023-09-23 12:50:59 +0530

    mobile view settings

--- src/components/Chatbot/Chatbot.js
+++ src/components/Chatbot/Chatbot.js
@@ -5,22 +5,21 @@ import 'react-chatbot-kit/build/main.css'
 import config from '../../chatbot/config';
 import MessageParser from '../../chatbot/MessageParser';
 import ActionProvider from '../../chatbot/ActionProvider';
-
+import './Chatbot.css'
 export default function Chatbot () {
   useEffect(() => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
-    <div style={{
-      display: "flex",
-      justifyContent: 'center',
-    }}>
+    <div style= {{display: 'flex', justifyContent: 'center', alignItems: 'center',height: '100vh'}}>
+      <div className = "react-chatbot-kit-chat-container">
       <Bot
         config={config}
         messageParser={MessageParser}
         actionProvider={ActionProvider}
       />
+    </div>
     </div>
   )
 }
