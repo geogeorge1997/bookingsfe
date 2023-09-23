350d64d2dbbd69f8660179494383587738dc5542
geogeorge1997 <igeogeorge1997@gmail.com>
2023-09-23 10:53:06 +0530

    router chnages

--- src/router/RouterItems.js
+++ src/router/RouterItems.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import Dashboard from '../components/Dashboard/Dashboard'
-import Preferences from '../components/Preferences/Preferences'
-import Login from '../components/Login/Login'
+import Chatbot from '../components/Chatbot/Chatbot'
+import Enroll from '../components/Enroll/Enroll'
+import Response from '../components/Response/Response'
 
 function RouterItems () {
-  const tokenValue = useSelector((state) => state.loginReducer.token)
-  console.log('Token Value - ', tokenValue)
-  if (!tokenValue) {
-    return (
-        <Login />
-    )
-  }
   return (
     <BrowserRouter>
         <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/preferences" element={<Preferences />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/" element={<Enroll />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/response" element={<Response />} />
         </Routes>
       </BrowserRouter>
   )
